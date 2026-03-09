482f789732a324c5f02b15070d8cc2c66d16c6ef
Nathan Grennan <github@cygnusx-1.org>
2026-03-09 14:09:27 -0700

    Fixed App crashes when opening a thread in the last subreddit in the sub list #281

--- app/src/main/java/me/edgan/redditslide/Activities/MainActivity.java
+++ app/src/main/java/me/edgan/redditslide/Activities/MainActivity.java
@@ -432,7 +432,8 @@ public class MainActivity extends BaseActivity
          * domain, the frontpage, or /r/all. If the subreddit has a "." in it, we know it's a domain
          * because subreddits aren't allowed to have hard-stops in the name.
          */
-        if (Authentication.didOnline && usedArray != null) {
+        if (Authentication.didOnline && usedArray != null && pager != null
+                && pager.getCurrentItem() < usedArray.size()) {
             final String subreddit = usedArray.get(pager.getCurrentItem());
 
             if (subreddit.contains("/m/")
